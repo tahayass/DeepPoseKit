45d411102b09fcabc0335697b5b5f33ca7d5e3eb
tahayass <ttahayyass@gmail.com>
2023-04-06 16:36:38 +0200

    fix

--- deepposekit/io/DLCDataGenerator.py
+++ deepposekit/io/DLCDataGenerator.py
@@ -84,8 +84,8 @@ class DLCDataGenerator(BaseGenerator):
             print(image_shape)
             coords = []
             for part in self.bodyparts:
-                x = (row[(self.scorer, part, "x")])
-                y = (row[(self.scorer, part, "y")])
+                x = (row[(self.scorer, part, "x")]*image_shape[1])/self.resize[1]
+                y = ((image_shape[0]-row[(self.scorer, part, "y")])*image_shape[0])/self.resize[0]
                 coords.append([x, y])
             coords = np.array(coords)
             keypoints.append(coords)
