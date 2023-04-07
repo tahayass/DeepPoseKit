4e49996ff157293d8ded2679ca71463cfd677fed
tahayass <ttahayyass@gmail.com>
2023-04-07 10:24:33 +0200

    fix

--- deepposekit/io/TrainingGenerator.py
+++ deepposekit/io/TrainingGenerator.py
@@ -195,9 +195,9 @@ class TrainingGenerator(Sequence):
     def __len__(self):
         """The number of batches per epoch"""
         if self.validation:
-            return float(self.n_validation) // self.batch_size
+            return int(float(self.n_validation) // self.batch_size)
         else:
-            return float(self.n_train) // self.batch_size
+            return int(float(self.n_train) // self.batch_size)
 
     def __call__(self, n_outputs=1, batch_size=32, validation=False, confidence=True):
         """ Sets the number of outputs and the batch size
